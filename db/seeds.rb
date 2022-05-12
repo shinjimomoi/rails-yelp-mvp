da5c1da02ab640e6e57200b48955ca0c943ea41b
Ricardo Momoi <shinjimomoi@gmail.com>
2022-05-13 01:41:46 +0900

    Commiting

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+5.times do
+  Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone_in_e164,
+    category: Restaurant::CATEGORIES.sample
+  )
+  puts "...created #{Restaurant.count} pets."
+end
